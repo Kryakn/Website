a0910de7ec07bfac68860b2cdcb7cb0cc8a500fd
Kryakn <aryanshrivastav150@gmail.com>
2026-08-02 13:21:19 +0530

    feat(backend): enable CORS for frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 
 const healthRoutes = require("./routes/healthRoutes");
-
 const authRoutes = require("./routes/authRoutes");
+
 const app = express();
+
+app.use(
+    cors({
+        origin: [
+            "http://127.0.0.1:5500",
+            "http://localhost:5500",
+        ],
+    })
+);
 
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);
-
 app.use("/api/auth", authRoutes);
+
 module.exports = app;
